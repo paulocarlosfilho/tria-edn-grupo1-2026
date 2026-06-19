230b20784b390d173ee15394c3222c967e3126b0
Paulo Carlos <paulocarlosfilho@gmail.com>
2026-06-19 10:09:17 -0300

    Feat: Criando o variables.tf

---
+++ infra-terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "nome_grupo" {
+  type    = string
+  default = "Grupo-1" # Altere para o seu número de grupo correto
+}
+
+variable "nome_projeto" {
+  type    = string
+  default = "Tria"
+}
